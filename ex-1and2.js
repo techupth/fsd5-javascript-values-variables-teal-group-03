655ba9cb30f395d07a931e63caafdcb58a465c79
tanasri <tanawan95@gmail.com>
2023-11-23 11:26:39 +0700

    completed exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let adress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const receipt = 15000;
+const numOfOperatingRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+let receipt2 = 15000;
+let numOfOperatingRestaurant2 = 1500;
+
+[adress, receipt2, numOfOperatingRestaurant2] = [
+  "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641",
+  5000,
+  2500,
+];
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,6 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
-
+let calFahrenheit = celsius * 1.8 + 32;
 // Start coding here;
 
-let fahrenheit;
+let fahrenheit = calFahrenheit;
